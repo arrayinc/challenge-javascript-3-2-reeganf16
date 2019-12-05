0b5b9f3f10978bca6178b462f4693cfe59f9123c
reeganford <rmford1695@gmail.com>
2019-12-05 14:09:27 -0700

    Finished challenge #2

--- answer.js
+++ answer.js
@@ -1,1 +1,14 @@
 // *** YOUR ANSWER BELOW ***
+
+let word = ['thumb', 'shoe', 'knee', 'door', 'hive', 'sticks', 'heaven', 'pate', 'spine', 'shin']
+
+function thisOldMan(verseN){
+    for(let i = 1; i <= verseN; i++) {
+        let verseWord = word[i-1];
+        console.log(`This old man, he played ${i}. 
+        He played knick knack on my ${verseWord}. With a knick-knack, paddy-whack, give your dog a bone.
+        This old man came rolling home.`)
+    }
+}
+
+(thisOldMan(10));
